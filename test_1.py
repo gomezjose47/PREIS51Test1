adeb9f68c7abef8c23b1d40e3136d6389562f196
Gomezjose47 <gomezjose47@mail.fresnostate.edu>
2020-11-05 17:10:39 -0800

    finish code

--- test_1.py
+++ test_1.py
@@ -40,3 +40,36 @@ else
 
 main
 """
+
+def option 1():
+   return 100 * 10
+
+def option2():
+   amount = 1
+   list 1 = []
+   for i in range (0 , 10):
+      list1.addend(amount)
+      amount *= 2
+   print("list1", list1)
+   total = sum (list1)
+   return total
+
+def main():
+ answer = ""
+ var1 = option1 #1000
+ var2 = option2 #1023
+ print("from main: var1", var1 "var2", var2)
+ if var1 == var2:
+    answer = "Option1 and option 2 pays the same"
+if var1 < var2:
+   answer = "Option 2 is better"
+else:
+   answer = "Option 1 is better"
+print(answer)
+
+var1 = option1()
+var2 = option2()
+print(var1)
+print (var2)
+
+main()
